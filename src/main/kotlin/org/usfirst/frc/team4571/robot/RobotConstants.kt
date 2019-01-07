f4dbfe9f67b1dcb291170a79f9db1bc79ca9aeb8
Mahim <mahim216@gmail.com>
2019-01-07 16:28:49 -0500

    Create RobotConstants.kt

---
+++ src/main/kotlin/org/usfirst/frc/team4571/robot/RobotConstants.kt
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team4571.robot
+
+object RobotConstants {
+    object Controllers {
+        const val LEFT_STICK = 0
+        const val RIGHT_STICK = 1
+    }
+
+    object DRIVE {
+        const val TOP_LEFT_MOTOR = 2
+        const val BOTTOM_LEFT_MOTOR = 1
+        const val TOP_RIGHT_MOTOR = 3
+        const val BOTTOM_RIGHT_MOTOR = 4
+    }
+}
