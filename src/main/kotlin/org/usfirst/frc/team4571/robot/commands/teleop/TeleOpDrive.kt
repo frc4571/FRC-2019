d242a9bad89e5434bab9b0b18b67ddced0f60e94
Mahim <mahim216@gmail.com>
2019-01-07 16:37:12 -0500

    logging values

--- src/main/kotlin/org/usfirst/frc/team4571/robot/commands/teleop/TeleOpDrive.kt
+++ src/main/kotlin/org/usfirst/frc/team4571/robot/commands/teleop/TeleOpDrive.kt
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team4571.robot.commands.teleop
 
 import edu.wpi.first.wpilibj.command.Command
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import org.usfirst.frc.team4571.robot.Robot
 import org.usfirst.frc.team4571.robot.subsystems.DriveSystem
 
@@ -9,8 +10,24 @@ object TeleOpDrive : Command() {
         requires(DriveSystem)
     }
 
-    override fun execute() =
+    private fun log() {
+        SmartDashboard.putNumber("Left Joystick", Robot.leftStick.yAxis)
+        SmartDashboard.putNumber("Right Joystick", Robot.rightStick.yAxis)
+
+        SmartDashboard.putNumber("Top Left Motor Output",
+                                 DriveSystem.topLeftMotorSpeed)
+        SmartDashboard.putNumber("Bottom Left Motor Output",
+                                 DriveSystem.bottomLeftMotorSpeed)
+        SmartDashboard.putNumber("Top Right Motor Output",
+                                 DriveSystem.topRightMotorSpeed)
+        SmartDashboard.putNumber("Bottom Right Motor Output",
+                                 DriveSystem.bottomRightMotorSpeed)
+    }
+
+    override fun execute() {
         DriveSystem.drive(Robot.leftStick.yAxis, Robot.rightStick.yAxis)
+        log()
+    }
 
     override fun isFinished(): Boolean = false
 
