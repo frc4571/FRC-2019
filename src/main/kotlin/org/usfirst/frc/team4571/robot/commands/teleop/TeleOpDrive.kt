ebb127a4ade47f088d858c0392dda0d607511ae2
Mahim <mahim216@gmail.com>
2019-01-20 14:41:35 -0500

    Update TeleOpDrive.kt

--- src/main/kotlin/org/usfirst/frc/team4571/robot/commands/teleop/TeleOpDrive.kt
+++ src/main/kotlin/org/usfirst/frc/team4571/robot/commands/teleop/TeleOpDrive.kt
@@ -14,19 +14,22 @@ object TeleOpDrive : Command() {
         SmartDashboard.putNumber("Left Joystick", Robot.leftStick.yAxis)
         SmartDashboard.putNumber("Right Joystick", Robot.rightStick.yAxis)
 
-        SmartDashboard.putNumber("Left Follower Motor Output",
-                                 DriveSystem.leftFollowerSpeed)
-        SmartDashboard.putNumber("Left Master Motor Output",
-                                 DriveSystem.leftMasterSpeed)
-        SmartDashboard.putNumber("Right Master Motor Output",
-                                 DriveSystem.rightMasterSpeed)
-        SmartDashboard.putNumber("Right Follower Motor Output",
-                                 DriveSystem.rightFollowerSpeed)
+//        SmartDashboard.putNumber("Left Follower Motor Output",
+//                                 DriveSystem.leftFollowerSpeed)
+//        SmartDashboard.putNumber("Left Master Motor Output",
+//                                 DriveSystem.leftMasterSpeed)
+//        SmartDashboard.putNumber("Right Master Motor Output",
+//                                 DriveSystem.rightMasterSpeed)
+//        SmartDashboard.putNumber("Right Follower Motor Output",
+//                                 DriveSystem.rightFollowerSpeed)
 
         SmartDashboard.putNumber("Right encoder Tick",
                                  DriveSystem.rightEncoderTick.toDouble())
         SmartDashboard.putNumber("Left Encoder Tick",
                                  DriveSystem.leftEncoderTick.toDouble())
+
+        SmartDashboard.putNumber("Right Distance", DriveSystem.rightDistance)
+        SmartDashboard.putNumber("Left Distance", DriveSystem.leftDistance)
     }
 
     override fun execute() {
