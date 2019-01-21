a56cae416c5b127e11c5236ceaf079896e7f33a5
Mahim <mahim216@gmail.com>
2019-01-21 04:25:56 -0500

    logging speed

--- src/main/kotlin/org/usfirst/frc/team4571/robot/commands/teleop/TeleOpDrive.kt
+++ src/main/kotlin/org/usfirst/frc/team4571/robot/commands/teleop/TeleOpDrive.kt
@@ -2,34 +2,34 @@ package org.usfirst.frc.team4571.robot.commands.teleop
 
 import edu.wpi.first.wpilibj.command.Command
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
+import org.usfirst.frc.team4571.robot.Constants
 import org.usfirst.frc.team4571.robot.Robot
 import org.usfirst.frc.team4571.robot.subsystems.DriveSystem
 
 object TeleOpDrive : Command() {
+    private var prevLeftDistance = 0.0
+    private var prevRightDistance = 0.0
+
     init {
         requires(DriveSystem)
     }
 
     private fun log() {
-        SmartDashboard.putNumber("Left Joystick", Robot.leftStick.yAxis)
-        SmartDashboard.putNumber("Right Joystick", Robot.rightStick.yAxis)
+        SmartDashboard.putNumber("Right Distance",
+                                 DriveSystem.rightDistance(Constants.Unit.Feet))
+        SmartDashboard.putNumber("Left Distance",
+                                 DriveSystem.leftDistance(Constants.Unit.Feet))
 
-//        SmartDashboard.putNumber("Left Follower Motor Output",
-//                                 DriveSystem.leftFollowerSpeed)
-//        SmartDashboard.putNumber("Left Master Motor Output",
-//                                 DriveSystem.leftMasterSpeed)
-//        SmartDashboard.putNumber("Right Master Motor Output",
-//                                 DriveSystem.rightMasterSpeed)
-//        SmartDashboard.putNumber("Right Follower Motor Output",
-//                                 DriveSystem.rightFollowerSpeed)
+        val leftSpeed = (DriveSystem.leftDistance(Constants.Unit.Feet) -
+                prevLeftDistance) / Constants.period
+        val rightSpeed = (DriveSystem.rightDistance(Constants.Unit.Feet) -
+                prevLeftDistance) / Constants.period
 
-        SmartDashboard.putNumber("Right encoder Tick",
-                                 DriveSystem.rightEncoderTick.toDouble())
-        SmartDashboard.putNumber("Left Encoder Tick",
-                                 DriveSystem.leftEncoderTick.toDouble())
+        SmartDashboard.putNumber("Left Velocity (ft/s)", leftSpeed)
+        SmartDashboard.putNumber("Right Velocity (ft/s)", rightSpeed)
 
-        SmartDashboard.putNumber("Right Distance", DriveSystem.rightDistance)
-        SmartDashboard.putNumber("Left Distance", DriveSystem.leftDistance)
+        prevLeftDistance = DriveSystem.leftDistance(Constants.Unit.Feet)
+        prevRightDistance = DriveSystem.rightDistance(Constants.Unit.Feet)
     }
 
     override fun execute() {
@@ -42,5 +42,7 @@ object TeleOpDrive : Command() {
     override fun end() {
         DriveSystem.stop()
         DriveSystem.resetEncoders()
+        prevLeftDistance = 0.0
+        prevRightDistance = 0.0
     }
 }
