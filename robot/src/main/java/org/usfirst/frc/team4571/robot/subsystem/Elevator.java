1254ec46af9545aa87739ee7053cd1c4229b216a
Alexandra <39199730+alexandraabrego1@users.noreply.github.com>
2019-02-12 18:35:38 -0500

    added to Elevator()

--- robot/src/main/java/org/usfirst/frc/team4571/robot/subsystem/Elevator.java
+++ robot/src/main/java/org/usfirst/frc/team4571/robot/subsystem/Elevator.java
@@ -1,15 +1,26 @@
 package org.usfirst.frc.team4571.robot.subsystem;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.usfirst.frc.team4571.robot.command.teleop.TeleOpElevator;
 
 public class Elevator extends Subsystem {
     private static Elevator instance;
-    private WPI_TalonSRX elevatorMotor;
+    private TalonSRX elevatorMotor, toplevatorMotor;
+
 
     private Elevator() {
+        this.elevatorMotor = new TalonSRX(RobotMap.ELEVATOR_MOTOR);
+        this.topelevatorMotor= new TalonSRX(RobotMap.TOPELEVATOR_MOTOR);
 
+        this.elevatorMotor.setExperation(Robot.DEFAULT_PERIOD);
+        this.topelevatorMotor.setExperation(Robot.DEFAULT_PERIOD);
+
+        this.elevatorMotor.setSafteyEnabled(false);
+        this.topelevatorMotor.setSafteyEnabled(false);
+
+        this.elevatorMotor.satNeutralMode(NeutralMode.Brake);
+        this.topelevatorMotor.setNeutralMode(NeutralMode.Brake);
     }
 
     @Override
