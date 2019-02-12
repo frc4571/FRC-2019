f61c9878b6b2f29efc579d31e10ac053a23427ef
Alexandra <39199730+alexandraabrego1@users.noreply.github.com>
2019-02-12 17:31:11 -0500

    adding motor

--- robot/src/main/java/org/usfirst/frc/team4571/robot/subsystem/Elevator.java
+++ robot/src/main/java/org/usfirst/frc/team4571/robot/subsystem/Elevator.java
@@ -1,10 +1,12 @@
 package org.usfirst.frc.team4571.robot.subsystem;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.usfirst.frc.team4571.robot.command.teleop.TeleOpElevator;
 
 public class Elevator extends Subsystem {
     private static Elevator instance;
+    private WPI_TalonSRX elevatorMotor;
 
     private Elevator() {
 
