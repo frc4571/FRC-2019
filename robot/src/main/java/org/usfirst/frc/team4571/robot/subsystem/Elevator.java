ec13cf8852ebd5ed90f198ad79b9076ed38e5b09
Mahim <mahim216@gmail.com>
2019-02-14 17:48:34 -0500

    fixed method names

--- robot/src/main/java/org/usfirst/frc/team4571/robot/subsystem/Elevator.java
+++ robot/src/main/java/org/usfirst/frc/team4571/robot/subsystem/Elevator.java
@@ -38,10 +38,10 @@ public class Elevator extends Subsystem {
     public void setTopMotor(double value) {
         topMotor.set(ControlMode.PercentOutput, value);
     }
-     public void StopBaseMotor(){
+     public void stopBaseMotor(){
         baseMotor.set(ControlMode.PercentOutput, 0);
     }
-    public void StopTopMotor(){
+    public void stopTopMotor(){
         topMotor.set(ControlMode.PercentOutput,0);
     }
 }
