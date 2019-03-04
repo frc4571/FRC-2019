7f2fda7c87a146e81c1b4bb91a47f3f71e1c1ef7
Mahim <mahim216@gmail.com>
2019-03-04 05:35:51 -0500

    making sure lines are < 80 char

--- rampage/ctre-motion/src/main/kotlin/com/rambots4571/rampage/ctre/motionprofile/Handler.kt
+++ rampage/ctre-motion/src/main/kotlin/com/rambots4571/rampage/ctre/motionprofile/Handler.kt
@@ -74,9 +74,9 @@ internal class Handler(
                 }
             ExecutionState.STARTED -> {
                 for (status in statuses) {
-                    if (status.btmBufferCnt <= Constants.Talon.MIN_POINTS_IN_TALON) {
+                    if (status.btmBufferCnt <=
+                            Constants.Talon.MIN_POINTS_IN_TALON)
                         readyToProgress = false
-                    }
                 }
                 if (readyToProgress) {
                     setMode(SetValueMotionProfile.Enable)
@@ -160,11 +160,10 @@ internal class Handler(
 
     private inner class PeriodicBufferProcessor : Runnable {
         override fun run() {
-            for (i in 0 until statuses.size) {
-                if (statuses[i].btmBufferCnt < Constants.Talon.MAX_BTM_BUFFER_COUNT) {
+            for (i in 0 until statuses.size)
+                if (statuses[i].btmBufferCnt <
+                        Constants.Talon.MAX_BTM_BUFFER_COUNT)
                     talons[i].processMotionProfileBuffer()
-                }
-            }
         }
     }
 
