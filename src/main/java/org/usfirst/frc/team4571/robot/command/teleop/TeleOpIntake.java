de5098c422e624a4ac6de63a252a6088cdab5cc0
Mahim <mahim216@gmail.com>
2019-03-08 12:26:52 -0500

    fixing intake problem (hopefully)

--- src/main/java/org/usfirst/frc/team4571/robot/command/teleop/TeleOpIntake.java
+++ src/main/java/org/usfirst/frc/team4571/robot/command/teleop/TeleOpIntake.java
@@ -1,14 +1,11 @@
 package org.usfirst.frc.team4571.robot.command.teleop;
 
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc.team4571.robot.Robot;
 import org.usfirst.frc.team4571.robot.subsystem.Intake;
 
 public class TeleOpIntake extends Command {
     private Intake intake = Intake.getInstance();
-    private JoystickButton intakeButton = Robot.leftStick.getButton1();
-    private JoystickButton outtakeButton = Robot.rightStick.getButton1();
 
     public TeleOpIntake() {
         requires(intake);
@@ -16,15 +13,7 @@ public class TeleOpIntake extends Command {
 
     @Override
     protected void execute() {
-        if (intakeButton.get() ^ outtakeButton.get()) {
-            if (intakeButton.get()) {
-                intake.setIntakePower(1);
-            } else if (outtakeButton.get()) {
-                intake.setIntakePower(-1);
-            }
-        } else {
-            intake.setIntakePower(0);
-        }
+        intake.setIntakePower(Robot.gamepad.getLeftTrigger() - Robot.gamepad.getRightTrigger());
         intake.setPulleyPower(Robot.gamepad.getRightYAxis());
     }
 
