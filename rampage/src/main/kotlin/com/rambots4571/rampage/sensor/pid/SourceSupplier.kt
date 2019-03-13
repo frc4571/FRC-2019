cd846f61d1eeaab179d127b20992596c7739fe0d
Mahim <mahim216@gmail.com>
2019-03-13 00:59:30 -0400

    fixing problem

--- rampage/src/main/kotlin/com/rambots4571/rampage/sensor/pid/SourceSupplier.kt
+++ rampage/src/main/kotlin/com/rambots4571/rampage/sensor/pid/SourceSupplier.kt
@@ -8,10 +8,12 @@ import java.util.function.DoubleSupplier
  * Class used to send values as a source without needing to implement PID
  * source all the time, inspired by team 166.
  */
-class SourceSupplier(private val sensorValue: DoubleSupplier,
-                     private var sourceType: PIDSourceType) : PIDSource {
+class SourceSupplier(
+    private val sensorValue: DoubleSupplier,
+    private var sourceType: PIDSourceType) : PIDSource {
 
-    constructor(sensorValue: DoubleSupplier) : this(func, PIDSourceType.kDisplacement)
+    constructor(sensorValue: DoubleSupplier) : this(
+            sensorValue, PIDSourceType.kDisplacement)
 
     override fun getPIDSourceType(): PIDSourceType = sourceType
 
