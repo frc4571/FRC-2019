41cd541fc3e2ad071beaa91f823572928e394a14
Mahim <mahim216@gmail.com>
2019-04-06 15:28:58 -0400

    slowing down pulley going down

--- src/main/java/com/rambots4571/deepspace/robot/subsystem/Intake.java
+++ src/main/java/com/rambots4571/deepspace/robot/subsystem/Intake.java
@@ -27,6 +27,7 @@ public class Intake extends Subsystem {
         pulleyMotor = new VictorSPX(Constants.Intake.PULLEY_MOTOR);
         pulleyMotor.setInverted(true);
         pulleyMotor.setNeutralMode(NeutralMode.Brake);
+        pulleyMotor.configNominalOutputReverse(-0.3, Constants.timeoutMs);
 
         hatchMotor = new VictorSPX(Constants.Intake.HATCH_MOTOR);
         hatchMotor.setNeutralMode(NeutralMode.Brake);
