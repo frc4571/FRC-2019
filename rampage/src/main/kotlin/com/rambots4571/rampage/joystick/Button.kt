667089fca3aa17dddd3968b684ed63498a5c6fd9
Mahim <mahim216@gmail.com>
2019-04-09 14:33:03 -0400

    using DoOnce class

--- rampage/src/main/kotlin/com/rambots4571/rampage/joystick/Button.kt
+++ rampage/src/main/kotlin/com/rambots4571/rampage/joystick/Button.kt
@@ -1,20 +1,19 @@
 package com.rambots4571.rampage.joystick
 
+import com.rambots4571.rampage.function.DoOnce
 import edu.wpi.first.wpilibj.GenericHID
 import edu.wpi.first.wpilibj.buttons.JoystickButton
+import java.util.function.Supplier
 
 class Button(joystick: GenericHID, buttonNumber: Int) :
         JoystickButton(joystick, buttonNumber) {
-    private var prevState: Boolean = false
-    private var currentState: Boolean = false
+    private val doOnce = DoOnce(Supplier(this::get), false)
 
     /**
      * Use this method when you want to do an action once inside a continuous
      * loop, such as the execute() method inside the Command class.
      */
     fun doOnce(action: Runnable) {
-        prevState = currentState
-        currentState = get()
-        if (currentState && !prevState) action.run()
+        doOnce.run(action)
     }
 }
